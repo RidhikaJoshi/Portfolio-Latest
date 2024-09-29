2421209215d55548cb6167e89efb86e5a484a325
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-09-29 21:12:42 +0530

    Refactor CSS and add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import { AboutMeComponent } from "./components/about-me";
+import { SkillsSectionComponent } from "./components/skills-section";
+import { ProjectsSection } from "./components/projects-section";
+import {EducationSection } from "./components/education-section";
+import {ContactSection} from "./components/contact-section";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="bg-black flex flex-col items-center justify-center">
+    <AboutMeComponent />
+    <SkillsSectionComponent/>
+    <ProjectsSection/>
+    <EducationSection/>
+    <ContactSection/>
+    </div>
   );
 }
