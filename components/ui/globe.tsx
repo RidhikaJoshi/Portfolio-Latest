f29891aa1edc4bbf0a5a10f274e09183c8cd2e7c
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-11-28 18:22:25 +0530

    Update ESLint configuration and refactor globe component for improved type handling

--- components/ui/globe.tsx
+++ components/ui/globe.tsx
@@ -182,7 +182,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
         return arcAlt * 1;
       })
       .arcStroke((e: object) => {
-        const { order } = e as { order: number };
+       // const { order } = e as { order: number };
         return [0.32, 0.28, 0.3][Math.round(Math.random() * 2)];
       })
       .arcDashLength(defaultProps.arcLength)
@@ -284,12 +284,12 @@ export function World(props: WorldProps) {
 }
 
 export function hexToRgb(hex: string) {
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function (m, r, g, b) {
     return r + r + g + g + b + b;
   });
 
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
     ? {
         r: parseInt(result[1], 16),
